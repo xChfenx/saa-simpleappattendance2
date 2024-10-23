e8b8ec0f5b2c11fcf2e2216fb81fc93e185e4cc9
nozredge <nozredge@proton.me>
2024-10-23 18:33:53 -0300

    feat: Añadir icono al componente de marcaje

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,24 +1,34 @@
 import Button from '@/components/Button';
-import styles from './page.module.css';
+import page_styles from './page.module.css';
+import registration_styles from '../styles/registration.module.css';
 import Registration from '@/components/Registration';
+import { QRScannerIcon } from '@/components/icons/QRScannerIcon';
 
 export default function Home(){
   return (
-    <div className={styles.body}>
-      <header className={styles.header}>
+    <div className={page_styles.body}>
+      <header className={page_styles.header}>
         <p>Iniciar sesión</p>
       </header>
-      <main className={styles.main}>
-        <Registration title='Marcaje'>
-          <Button button={{
+      <main className={page_styles.main}>
+        <Registration title='Marcaje' >
+          <div className={registration_styles.registration_content}>
+            <p>Acerca tu tarjeta al escaner</p>
+            <QRScannerIcon/>
+            <hr />
+          </div>
+          <div className={registration_styles.buttons_container}>
+            <Button button={{
             title: 'Usar PIN',
             color: '#333333',
             background: '#EBEBEB',
             height: '5rem',
             width: '20rem',
             }}/>
+          </div>
         </Registration>
       </main>
     </div>
   );
 }
+
