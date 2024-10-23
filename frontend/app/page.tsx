fdfdfe31d706b4bdbb806a69d2ddbc4ff9863a29
nozredge <nozredge@proton.me>
2024-10-23 03:46:19 -0300

    feat: Agregar componente de Marcaje

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,6 @@
+import Button from '@/components/Button';
 import styles from './page.module.css';
+import Registration from '@/components/Registration';
 
 export default function Home(){
   return (
@@ -7,6 +9,15 @@ export default function Home(){
         <p>Iniciar sesión</p>
       </header>
       <main className={styles.main}>
+        <Registration title='Marcaje'>
+          <Button button={{
+            title: 'Usar PIN',
+            color: '#333333',
+            background: '#EBEBEB',
+            height: '5rem',
+            width: '20rem',
+            }}/>
+        </Registration>
       </main>
     </div>
   );
