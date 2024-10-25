04a80ed57ddc777f97c7fa5d951ef540a04c7018
nozredge <nozredge@proton.me>
2024-10-25 19:14:43 -0300

    refactor: Cambiar estilos del componente marcaje

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,33 +1,31 @@
 import Button from '@/components/Button';
-import page_styles from './page.module.css';
-import registration_styles from '../styles/registration.module.css';
+import home_styles from './page.module.css';
+import registration_content_styles from '@/styles/registration_content.module.css';
 import Registration from '@/components/Registration';
 import { QRScannerIcon } from '@/components/icons/QRScannerIcon';
 
 export default function Home(){
   return (
-    <div className={page_styles.body}>
-      <header className={page_styles.header}>
+    <div className={home_styles.full_content}>
+      <header className={home_styles.full_content_header}>
         <p>Iniciar sesión</p>
       </header>
-      <main className={page_styles.main}>
+      <main className={home_styles.full_content_main}>
         
         <Registration title='Marcaje' >
-          <div className={registration_styles.registration_content}>
+          <div className={registration_content_styles.registration_content}>
             <p>Acerca tu tarjeta al escaner</p>
-            <div className={registration_styles.qr_icon}>
+            <div className={registration_content_styles.qr_icon}>
               <QRScannerIcon />
             </div>
             <hr />
-          </div>
-          <div className={registration_styles.buttons_container}>
             <Button button={{
             title: 'Usar PIN',
             color: '#333333',
             background: '#EBEBEB',
             height: '4rem'
             }}/>
-          </div>
+          </div> 
         </Registration>
       </main>
     </div>
