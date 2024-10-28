2aa7325ca1c162b303d123a64965097129828b7a
nozredge <nozredge@proton.me>
2024-10-27 21:01:27 -0300

    feat: Crear ruta para devolver un usuario de prueba

---
+++ nitro/server/api/users/index.get.ts
@@ -0,0 +1,18 @@
+export default eventHandler((event) => {
+  return [
+    {
+      "rut": "12345678-9",
+      "nombres": "Juan",
+      "apellidos": "Pérez González",
+      "correo": "juan.perez@ejemplo.com",
+      "clave": "contraseña123",
+      "area": "Desarrollo",
+      "cargo": "Programador",
+      "tipHorario": "Flexible",
+      "empresa": "Ejemplo S.A.",
+      "asistencia": "2023-11-22T10:00:00",
+      "qrCode": "https://ejemplo.com/qr/12345678-9",
+      "pin": 1234
+  }
+  ]
+});
