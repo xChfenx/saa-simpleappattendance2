7a71d76e8bbbb196cb0b6e330269f3a87ccad389
nozredge <nozredge@proton.me>
2024-11-03 14:59:39 -0300

    feat: Añadir ruta de api para obtener usuarios

--- nitro/server/api/users/index.get.ts
+++ nitro/server/api/users/index.get.ts
@@ -1,18 +1,3 @@
-export default eventHandler((event) => {
-  return [
-    {
-      "rut": "12345678-9",
-      "nombres": "Juan",
-      "apellidos": "Pérez González",
-      "correo": "juan.perez@ejemplo.com",
-      "clave": "contraseña123",
-      "area": "Desarrollo",
-      "cargo": "Programador",
-      "tipHorario": "Flexible",
-      "empresa": "Ejemplo S.A.",
-      "asistencia": "2023-11-22T10:00:00",
-      "qrCode": "https://ejemplo.com/qr/12345678-9",
-      "pin": 1234
-  }
-  ]
+export default eventHandler(async (event) => {
+  return await listUsers();
 });
