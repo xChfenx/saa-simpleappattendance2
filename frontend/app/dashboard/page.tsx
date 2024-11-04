66475aa53dbcfa94e40f6f141e09eea2d003e491
nozredge <nozredge@proton.me>
2024-11-03 23:32:08 -0300

    feat: Añadir botones de cerrar sesión, reportes y crear usuario

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import 'bulma/css/bulma.min.css';
-import nav_styles from '@/app/dashboard/page.module.css';
-
+import dashboard_styles from '@/app/dashboard/page.module.css';
 import { fetchUsers } from '../utils/data';
+import Link from 'next/link';
 
 export default async function Dashboard(){
 
@@ -9,60 +9,81 @@ export default async function Dashboard(){
 
   return (
     <>
-        <nav className={`navbar ${nav_styles.nav}`}>
-          <div className='container is-fluid'>
-            <div className='is-flex navbar-end'>
-              <p className='has-text-white has-text-centered is-align-content-center'>{`Bienvenido, ${users[0].nombres}`}</p>
+      <nav className={`navbar ${dashboard_styles.nav}`}>
+
+        {/* Mensaje de bienvenida */}
+          <div className="navbar-item is-expanded">
+            <div className='container has-text-centered'>
+              <p className='title has-text-white'>{`Bienvenido, ${users[0].nombres}`}</p>
             </div>
           </div>
         </nav>
 
-      {/* Table */}
-      <div className='container is-fluid'>
-        <div className='columns'>
-          <div className='column is-2'>
-            {/* Left side menu */}
-            <aside className="menu">
-              <p className="menu-label">General</p>
-              <ul className="menu-list">
-                <li><a>Reportes</a></li>
-                <li><a>Registrar usuario</a></li>
-              </ul>
-            </aside>
-          </div>
+      <div className='columns'>
+        <div className='column is-2'>
+          {/* Botones de Reportes y Crear usuario */}
+          <section className={`hero is-fullheight-with-navbar ${dashboard_styles.hero}`}>
+            <div className="hero-body">
+              <div className="container">
+                <div className="buttons">
+                  <div className="block">
+                    <Link href="#" className="button is-medium is-outlined">Reportes</Link>
+                  </div>
+                  <div className="block">
+                    <Link href="#" className="button is-medium">Crear usuario</Link>
+                  </div>
+                </div>
+              </div>
+            </div>
 
-          <div className='column is-align-items-stretch'>
-            <table className='table is-hoverable is-fullwidth'>
-              <thead>
-                <tr>
-                  <th>RUT</th>
-                  <th>Nombres</th>
-                  <th>Apellidos</th>
-                  <th>Correo</th>
-                  <th>Área</th>
-                  <th>Cargo</th>
-                  <th>Tipo Horario</th>
-                  <th>Empresa</th>
-                  <th>Asistencia</th>
-                </tr>
-              </thead>
-              <tbody>
-                {users.map((user: any) => (
-                <tr key={users.rut}>
-                  <th>{users[0].rut}</th>
-                  <td>{users[0].nombres}</td>
-                  <td>{users[0].apellidos}</td>
-                  <td>{users[0].correo}</td>
-                  <td>{users[0].area}</td>
-                  <td>{users[0].cargo}</td>
-                  <td>{users[0].tipoHorario}</td>
-                  <td>{users[0].empresa}</td>
-                  <td>{users[0].asistencia}</td>
-                </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+            {/* Botón de cerrar sesión */}
+            <div className="hero-foot">
+              <nav className="level">
+                <div className="level-item">
+                  <div className="buttons">
+                    <Link href="/" className="button is-light">
+                      <strong>Cerrar sesión</strong>
+                    </Link>
+                  </div>
+                </div>
+              </nav>
+            </div>
+            
+          </section>
+        </div>
+
+        {/* Tabla de usuarios */}
+        <div className='column is-align-items-stretch'>
+          <table className='table is-hoverable is-fullwidth'>
+            <thead>
+              <tr>
+                <th>RUT</th>
+                <th>Nombres</th>
+                <th>Apellidos</th>
+                <th>Correo</th>
+                <th>Área</th>
+                <th>Cargo</th>
+                <th>Tipo Horario</th>
+                <th>Empresa</th>
+                <th>Asistencia</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user: any) => (
+              <tr key={user.rut}>
+                <th>{user.rut}</th>
+                <td>{user.nombres}</td>
+                <td>{user.apellidos}</td>
+                <td>{user.correo}</td>
+                <td>{user.area}</td>
+                <td>{user.cargo}</td>
+                <td>{user.tipoHorario}</td>
+                <td>{user.empresa}</td>
+                <td>{user.asistencia}</td>
+              </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </>
