20c7e92a521dd8c965bbd65f7f81fae46ae3f0d0
nozredge <nozredge@proton.me>
2024-11-15 20:17:03 -0300

    feat: Agregar definición de usuario

---
+++ frontend/app/utils/definitions.ts
@@ -0,0 +1,14 @@
+export type User = {
+    rut: string;
+    nombres: string;
+    apellidos: string;
+    correo: string;
+    clave: string;
+    area: string;
+    cargo: string;
+    turno: string;
+    empresa: string;
+    qrCode: string;
+    pin: string;
+    rolId: number;
+};
